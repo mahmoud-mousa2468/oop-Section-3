23be893c2c28c6d1ada51370d3003e837304dffc
Mo <Mo@DESKTOP-7P4PPCV>
2025-01-07 17:09:45 +0200

    Interface Ex01

---
+++ Demo/Interface-Ex01/MyType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interface_Ex01
+{
+    internal class MyType : Program, IMyType ,IComparable,ICloneable
+    {
+        public double Salary{ set; get;}
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MyFun()
+        {
+            Console.WriteLine("Hello World :)");
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Demo
+﻿using Demo.Interface_Ex01;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region InterfaceEx01
+            // Interfaces : Reference Type
+            // Code Contract Between The Developer Who Write It And The Developer Who Use It
+
+            //IMyType myType = new IMyType(); // Invalid 
+
+            // Note : Can't Create Object From Any Interfaces
+
+            //MyType myType = new MyType();
+            //myType.Salary = 12000;
+            //Console.WriteLine(myType.Salary);
+            //myType.MyFun();
+
+            //IMyType myType;
+            //Ref-- > Can Refer To Object From Any Class Which Implemented The Interface 'IMyType'
+
+            //myType = new MyType();
+            //myType.Salary = 12000;
+            //Console.WriteLine(myType.Salary);
+            //myType.MyFun();
+            //myType.print();
+            #endregion
         }
     }
 }
