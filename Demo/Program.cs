715b579c601abb8055a0e85d2b7da36f1a0335a0
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-01-09 11:57:53 +0200

    Shallow Copy Vs Deep Copy

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,5 @@
-﻿using Demo.Interface_Ex02;
-using Demo.Interface_Ex03;
+﻿using System.Text;
+using Demo.Interface_Ex02;
 
 namespace Demo
 {
@@ -59,6 +59,160 @@ namespace Demo
 
             //IFlyable flyable = airplane;
             //flyable.Forward();
+            #endregion
+
+            #region Shallow Copy Vs Deep Copy
+            #region Arr Of Value Type
+            //int[] Arr01 = { 1, 2, 3 };
+            //int[] Arr02 = { 4, 5, 6 };
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = Arr01; //Shallow Copy
+            ////Copy Identity
+            //// {1,2,3} --> Has Two Ref Arr01,Arr02
+            //// {4,5,6} --> UnReachable Object
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0] = 100;
+
+            //Console.WriteLine(Arr01[0]);
+
+            //int[] Arr01 = { 1, 2, 3 };
+            //int[] Arr02 = { 4, 5, 6 };
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = (int[])Arr01.Clone(); //Deep Copy
+            ////Clone Method Will Copy The Object State Of The Caller
+            //// Assign The New object To Arr02 , Will Generate New Identity
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0] = 100;
+
+            //Console.WriteLine(Arr01[0]);
+            #endregion
+
+            #region Arr Of Ref Type
+
+            //string[] Arr01 = { "Ahmed", "Ali", "mo" };
+            //string[] Arr02 = { "medo", "marwan", "adam" };
+
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = Arr01; //Shallow Copy
+            //////Copy Identity
+            ////// { "Ahmed", "Ali", "mo" } --> Has Two Ref Arr01,Arr02
+            ////// { "medo", "marwan", "adam" } --> UnReachable Object
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0] = "Eslam";
+
+            //Console.WriteLine(Arr01[0]);
+
+
+            //string[] Arr01 = { "Ahmed", "Ali", "mo" };
+            //string[] Arr02 = { "medo", "marwan", "adam" };
+
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = (string[])Arr01.Clone(); //Deep Copy
+            ////Clone Method Will Copy The Object State Of The Caller
+            //// Assign The New object To Arr02 , Will Generate New Identity
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0] = "Eslam";
+
+            //Console.WriteLine(Arr01[0]);
+
+            #endregion
+
+            #region Arr Of StringBuilder
+
+            //StringBuilder[] Arr01 = new StringBuilder[]
+            //{ new StringBuilder("Ahmed"),
+            //  new StringBuilder("Ali"),
+            //  new StringBuilder("Omar")};
+            //StringBuilder[] Arr02 = new StringBuilder[]
+            //{ new StringBuilder("medo"),
+            //  new StringBuilder("eslam"),
+            //  new StringBuilder("tamer")};
+
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = Arr01; //Shallow Copy
+            //////Copy Identity
+            ////// { "Ahmed", "Ali", "mo" } --> Has Two Ref Arr01,Arr02
+            ////// { "medo", "marwan", "adam" } --> UnReachable Object
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0].Append("Ahmed Amin");
+
+            //Console.WriteLine(Arr01[0]);
+
+
+            //StringBuilder[] Arr01 = new StringBuilder[]
+            //{ new StringBuilder("Ahmed"),
+            //  new StringBuilder("Ali"),
+            //  new StringBuilder("Omar")};
+            //StringBuilder[] Arr02 = new StringBuilder[]
+            //{ new StringBuilder("medo"),
+            //  new StringBuilder("eslam"),
+            //  new StringBuilder("tamer")};
+
+
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            //Arr02 = (StringBuilder[])Arr01.Clone(); //Shallow Copy
+            //////Copy Identity
+            ////// { "Ahmed", "Ali", "mo" } --> Has Two Ref Arr01,Arr02
+            ////// { "medo", "marwan", "adam" } --> UnReachable Object
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Arr01: {Arr01.GetHashCode()}");
+            //Console.WriteLine($"Arr02: {Arr02.GetHashCode()}");
+
+            ////Identity (Address) + Object State [Data]
+
+            //Arr02[0].Append("Ahmed Amin");
+
+            //Console.WriteLine(Arr01[0]);
+
+            #endregion
             #endregion
         }
     }
